94bc4a06404f8b59838d6f23be340cfa5f7c7cf8
Peter Du <peterdu2@illinois.edu>
2022-10-03 10:23:21 -0500

    syncing parser and gif generators

--- AST_CrowdNav/ast/ast_parser.py
+++ AST_CrowdNav/ast/ast_parser.py
@@ -9,25 +9,25 @@ from simulators.dsrnn_coupled_simulator import DSRNNCoupledSimulator
 
 model_dirs = ['dsrnn_models/policy_summarization_10_humans/', 'dsrnn_models/policy_summarization_10_humans/']
 config_name = ['config', 'config']
-model_names = ['14000.pt', '20600.pt']
+model_names = ['14000.pt', '30800.pt']
 
 s_0 = []
-s_0.append([-5., 4., 7., -2.])
-s_0.append([-1.9764521104695284, 3.673416349134316])
-s_0.append([-1.3916318531059858, -4.311028515571641])
-s_0.append([5.16749983681399, -2.8436072010801905])
-s_0.append([0.382823965553917, 0.9261922797511946])
-s_0.append([-3.5008964915665364, -1.244077560533835])
+s_0.append([-5., -4., 7., 2.])
+s_0.append([0.9764521104695284, 5.673416349134316])
+s_0.append([-0.3916318531059858, -6.311028515571641])
+s_0.append([6.16749983681399, -1.8436072010801905])
+s_0.append([2.382823965553917, 0.9261922797511946])
+s_0.append([-1.7508964915665364, -4.244077560533835])
 s_0.append([1.8364308394984743, -4.473900760196651])
 s_0.append([5.561697860813725, 0.13723735189354574])
-s_0.append([4.213509392947646, 1.356352793759812])
-s_0.append([5.92327303238429, 1.5992114112982367])
+s_0.append([4.213509392947646, 4.356352793759812])
+s_0.append([4.92327303238429, 2.5992114112982367])
 s_0.append([-2.5587340425394998, 1.7638743741024])
 
 mode = 'OBSERVATION_NOISE'
 mode = 'DIRECT_ACTION'
 
-log_folder_name = 'ast_dsrnn_6'
+log_folder_name = 'ast_dsrnn_5'
 render_path = '/home/peter/policy_summarization/AST_CrowdNav/ast/results/data/' + log_folder_name + '/renders'
 
 if __name__ == '__main__':
@@ -55,13 +55,13 @@ if __name__ == '__main__':
             os.makedirs(save_render_path)
 
         sim.reset(s_0=s_0)
-        sim.render(save_render=False, render_path=save_render_path, pause=1000000.)
+        sim.render(save_render=True, render_path=save_render_path, pause=0.)
 
         done_traj = False
         for action in action_seq:
             sim.step(action.action)
             #print(sim.sim_infos)
-            sim.render(save_render=False, render_path=save_render_path, pause=0.)
+            sim.render(save_render=True, render_path=save_render_path, pause=0.)
             for state in sim.sim_infos:
                 if isinstance(state['info'], ReachGoal):
                     done_traj = True
